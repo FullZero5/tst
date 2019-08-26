c3d0cb6da5faa2d111bc6dcb32f36cfa4609df70
Viktor <fullzero5@gmail.com>
2019-08-26 22:23:50 +0000

    add vuex state,getters,action, mutation

--- src/store.js
+++ src/store.js
@@ -5,12 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+   data: [],
+  },
+  getters: {
+    getData: state => {
+      return state.data;
+    }
   },
   mutations: {
-
+   ADD_DATA: (state, item) => {
+    state.data.push(item);
+   }
   },
   actions: {
-
+   addToItems: ({ commit }, item) => commit("ADD_DATA", item),
   }
 })
