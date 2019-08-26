76a26376408aa105a3bc1a1d8b7864fa3f4c9ff9
Viktor <fullzero5@gmail.com>
2019-08-26 15:26:19 +0000

    add method search coordinates adn function calculate

--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -10,7 +10,8 @@
           id="source"
           aria-describedby="Начальная точка"
           placeholder="Пункт А"
-          v-model="sourceAddress">
+          v-model="sourceAddress"
+          @blur="searchCoordinates('source', sourceAddress)">
        </div>
        <div class="form-group col-12 col-md-4 px-1">
          <label for="source">{{TextPlaceholderDestinationAddress}}</label>
@@ -21,16 +22,18 @@
           aria-describedby="Конечная точка"
           placeholder="Пункт Б"
           v-model="destinationAddress"
+          @blur="searchCoordinates('destination', destinationAddress)"
         >
        </div>
        <div class="form-group col-12 col-md-2 px-1">
          <label>&nbsp;</label>
-         <button class="btn btn-primary w-100 is-primary pr-2">Go</button>
+         <button class="btn btn-primary w-100 is-primary pr-2"
+         @click.prevent="calculate">Go</button>
        </div>
        <div class="form-group col-12 col-md-2 px-1"> 
         <label>&nbsp;</label>
         <button class="btn btn-primary w-100 is-primary pr-2"
-        @click.prevent="ClearInput"
+        @click.prevent="clearInput"
         >Clear</button>
        </div>
      </div>
@@ -39,6 +42,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'InputsComponents',
   props: {
@@ -70,9 +75,15 @@ export default {
     }
   },
   methods: {
-   ClearInput: function(){
+   clearInput(){
       this.sourceAddress = this.destinationAddress = "";
+   },
+   searchCoordinates(ref, address){
+     console.log(ref, address)
+   },
+   calculate(){
+     
    }
-  }
+  },
 }
 </script>
