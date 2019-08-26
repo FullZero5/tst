8ac6209e6685c20ec9ee56f8f5bfa7876d2fd65f
Viktor <fullzero5@gmail.com>
2019-08-26 14:52:26 +0000

    add button create inputs, add interator for generete path

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
 <template>
-  <div id="app">
-    <Inputs/>
+  <div id="app" class="container">
+    <section class="column">
+       <button @click="addInput" class="btn btn-primary">+</button>
+    </section>  
+    <section class="column">
+     <Inputs
+      v-for="(path, index) in paths"
+          :key="index"
+          :TextPlaceholderSourceAddress="path.titleSourceAddress"
+          :TextPlaceholderDestinationAddress="path.titleDestinationAddress"
+     />
+    </section>
   </div>
 </template>
 
 <script>
-import Inputs from './components/Inputs.vue'
+import Inputs from './components/Inputs.vue';
+
+function makeIterator(array) {
+  let nextIndex = 0;
+  return {
+    next: function() {
+      return nextIndex < array.length
+        ? { value: array[nextIndex++], done: false }
+        : { done: true };
+    }
+  };
+}
+
+let letter = makeIterator(["A", "B", "C", "D", "I","F", "G", "K"]);
 
 export default {
   name: 'app',
   components: {
     Inputs
+  },
+  data() {
+    return {
+      paths: [
+        {
+          titleSourceAddress: "Пункт-"+letter.next().value,
+          sourceAddress: "Краснодар",
+          titleDestinationAddress: "Пункт-"+letter.next().value,
+          destinationAddress: "Москва"
+        }
+      ]
+    };
+  },
+  methods: {
+    addInput(){
+      if(this.paths.length<4){
+        this.paths.push({
+          titleSourceAddress: "Пункт-"+letter.next().value,
+          sourceAddress: "Красноадр",
+          titleDestinationAddress: "Пункт-"+letter.next().value,
+          destinationAddress: "Москва"
+        });
+      }
+    }
   }
 }
 </script>
