380f6a41665709153a8bcd9958857b815c8a6f96
Viktor <fullzero5@gmail.com>
2019-08-26 14:31:58 +0000

    add grid

--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -1,6 +1,20 @@
 <template>
  <section>
- <section>
+   <form role="form">
+     <div class="row">
+       <div class="form-group col-12 col-md-4 px-1">
+         <label for="source">Пункт А</label>
+       </div>
+       <div class="form-group col-12 col-md-4 px-1">
+         <label for="source">Пункт Б</label>
+       </div>
+       <div class="form-group col-12 col-md-4 px-1">
+         <label>&nbsp;</label>
+         <button class="btn btn-primary w-100 is-primary pr-2">Go</button>
+       </div>
+     </div>
+   </form>
+   <section>
 </template>
 
 <script>
