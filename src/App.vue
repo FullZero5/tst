5ff719389f1eb30872a8bfd96e3d39aef5883e3a
Viktor <fullzero5@gmail.com>
2019-08-28 14:57:11 +0000

    remove redundancy

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
   <div id="app" class="container ">
     <section class="column text-left">
-       <button @click="addInput" class="btn btn-primary ">+</button>
+       <button @click.once="addDistanceComponents" class="btn btn-primary ">+</button>
     </section>  
     <section class="column">
-     <Inputs
-      v-for="(path, index) in paths"
-          :key="index"
-          :TextPlaceholderSourceAddress="path.titleSourceAddress"
-          :TextPlaceholderDestinationAddress="path.titleDestinationAddress"
-          :sourceAddress="path.sourceAddress"
-          :destinationAddress="path.destinationAddress"
+     <Inputs  v-for="(path, index) in paths"  :key="index"
      />
     </section>
     <section class="column">
@@ -18,27 +12,13 @@
       <ul>
         <li  v-for="(log, index) in getData" :key="index"> {{log}}</li>
       </ul>
-   
     </section>
   </div>
 </template>
 
 <script>
-import Inputs from './components/Inputs.vue';
+import Inputs from './components/DistanceComponents.vue';
 import { mapGetters } from "vuex";
-
-function makeIterator(array) {
-  let nextIndex = 0;
-  return {
-    next: function() {
-      return nextIndex < array.length
-        ? { value: array[nextIndex++], done: false }
-        : { done: true };
-    }
-  };
-}
-
-let letter = makeIterator(["A", "B", "C", "D", "I","F", "G", "K"]);
 
 export default {
   name: 'app',
@@ -47,29 +27,18 @@ export default {
   },
   data() {
     return {
-      paths: [
-        {
-          titleSourceAddress: "Пункт-"+letter.next().value,
-          sourceAddress: "Краснодар",
-          titleDestinationAddress: "Пункт-"+letter.next().value,
-          destinationAddress: "Москва"
-        }
-      ]
+      paths: []
     };
   },
   computed: {
     ...mapGetters(["getData"])
   },
+  created() {
+    this.addDistanceComponents();
+  },
   methods: {
-    addInput(){
-      if(this.paths.length<4){
-        this.paths.push({
-          titleSourceAddress: "Пункт-"+letter.next().value,
-          sourceAddress: "Темрюк",
-          titleDestinationAddress: "Пункт-"+letter.next().value,
-          destinationAddress: "Сочи"
-        });
-      }
+    addDistanceComponents(){
+      this.paths.push({ sourceAddress: " ",destinationAddress: " "});
     }
   }
 }
